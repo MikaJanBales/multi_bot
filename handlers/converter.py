7fccaaa99ef3230eb75d225c100368a3487f6f83
MikaJanBales <114147741+MikaJanBales@users.noreply.github.com>
2023-04-25 10:12:55 +0300

    0.2

--- handlers/converter.py
+++ handlers/converter.py
@@ -62,19 +62,26 @@ async def get_convert_sum_wallet(message: types.Message, state: FSMContext):
 # обработчик конвиртируемых валют и выдача результата конвертации
 @dp.message_handler(state=Wallets.from_to_wallet)
 async def get_convert_from_to_wallet(message: types.Message, state: FSMContext):
-    # сохранение ввода(пара конвертируемых валют) пользователя
+    # сохранение ввода(пары конвертируемых валют) пользователя
     answer = message.text
 
     # обработка кнопок
     if answer != "Другое значение.":
         answer = answer.upper().split("/")
+
+        # обработка ошибки ввода неправильного формата
+        if len(answer) < 2:
+            mess = "Неправильный формат.\nНапишите пару валют в виде <1 валюта>/<2 валюта>"
+            await message.answer(mess)
+            await Wallets.from_to_wallet.set()
+            return
         await state.update_data(from_to_wallet=answer)
         data = await state.get_data()
         amount = data.get("sum_wallet")
         from_wallet = data.get("from_to_wallet")[0]
         to_wallet = data.get("from_to_wallet")[1]
 
-        # обработка ошибки неправильного ввода пары валют или ввод несуществующих валют
+        # обработка ошибки ввода несуществующих валют
         try:
             res = await convert(amount, from_wallet, to_wallet)
             await message.answer(
@@ -84,10 +91,10 @@ async def get_convert_from_to_wallet(message: types.Message, state: FSMContext):
             mess = "Выбери, пожалуйста, функцию, чем хочешь воспользоваться."
             markup = button_for_menu()
             await message.reply(mess, reply_markup=markup)
-        except:
+        except UnboundLocalError:
             mess = "Такая валюта не поддерживается."
             await message.reply(mess)
-            await Wallets.sum_wallet.set()
+            await Wallets.from_to_wallet.set()
     else:
         mess = "Напишите пару валют в виде <1 валюта>/<2 валюта>"
         await message.answer(mess)
